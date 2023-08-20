43becbba83bce968ae99a488b17fcf355bb43876
Herr Mannelig <j.geova.89@gmail.com>
2023-08-20 15:27:18 -0600

    Proyecto completo

--- client/server/build/keys.js
+++ client/server/build/keys.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
         host: 'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'l028bvyrdvbukucz1',
+        user: 'l028bvyrdvbukucz',
         password: 'qa6j3qq86p62drl2',
         database: 'cr0g62ryusrx68zg'
     }
