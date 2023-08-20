113bb86baff8c469df54b84df2b34d389c794d9a
Herr Mannelig <j.geova.89@gmail.com>
2023-08-20 15:21:32 -0600

    Proyecto completo

--- client/server/src/keys.ts
+++ client/server/src/keys.ts
@@ -1,7 +1,7 @@
 export default{
     database:{
-        host:'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.cn',
-        user:'l028bvyrdvbukucz',
+        host:'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user:'l028bvyrdvbukucz1',
         password:'qa6j3qq86p62drl2',
         database:'cr0g62ryusrx68zg'
     }
